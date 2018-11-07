9914ed9d7349f6b5651f9021b3fda2dae45366eb
Raschad <Raschad@yandex.ru>
2018-11-07 20:51:13 +0300

    rr

--- rr.mk
+++ rr.mk
@@ -1,5 +1,5 @@
 # Inherit AICP common Phone stuff.
-$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 $(call inherit-product, device/sony/honami/full_honami.mk)
 
