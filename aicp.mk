d9b9963f6bab517f26b697a72d03c71c36ba43b5
Raschad <Raschad@yandex.ru>
2018-11-30 23:09:37 +0300

    vendor match fix

--- aicp.mk
+++ aicp.mk
@@ -4,8 +4,9 @@ $(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 $(call inherit-product, device/sony/honami/full_honami.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6903
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/C6903/C6903:5.1.1/14.6.A.1.236/2031203603:user/release-keys
 PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="C6903-user 5.1.1 14.6.A.1.236 2031203603 release-keys"
+
+BUILD_FINGERPRINT := Sony/C6903/C6903:5.1.1/14.6.A.1.236/2031203603:user/release-keys
 
 PRODUCT_NAME := aicp_honami
 PRODUCT_DEVICE := honami
