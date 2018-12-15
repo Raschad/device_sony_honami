4688a59a81ec9e0b70a9c0a9b26f2b83693688fc
Raschad <Raschad@yandex.ru>
2018-12-16 00:26:57 +0300

    aosp-fy

--- aosp.mk
+++ aosp.mk
@@ -1,5 +1,5 @@
 # Inherit AICP common Phone stuff.
-$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 $(call inherit-product, device/sony/honami/full_honami.mk)
 
@@ -8,7 +8,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="C6903-user 5.1.1 14.6.A.1.23
 
 BUILD_FINGERPRINT := Sony/C6903/C6903:5.1.1/14.6.A.1.236/2031203603:user/release-keys
 
-PRODUCT_NAME := havoc_honami
+PRODUCT_NAME := aosp_honami
 PRODUCT_DEVICE := honami
 
 # AICP Device Maintainer
