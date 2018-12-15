80829c31de0eaa8f4f7fb930ac50333fb053f77d
Raschad <Raschad@yandex.ru>
2018-12-15 21:34:21 +0300

    havoc-fy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,7 +26,7 @@ TARGET_SPECIFIC_HEADER_PATH += device/sony/honami/include
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/sony/honami/bluetooth
 
 # Kernel properties
-TARGET_KERNEL_CONFIG := carbon_rhine_honami_row_defconfig
+TARGET_KERNEL_CONFIG := havoc_rhine_honami_row_defconfig
 
 # Partition information
 BOARD_VOLD_MAX_PARTITIONS := 26
