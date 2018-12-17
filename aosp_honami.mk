198d686ded13b8b69d7dac40d3b9e4bb6c079f59
Raschad <Raschad@yandex.ru>
2018-12-17 08:19:18 +0300

    Update aosp_honami.mk

--- aosp_honami.mk
+++ aosp_honami.mk
@@ -3,6 +3,7 @@ $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 $(call inherit-product, device/sony/honami/full_honami.mk)
 
+
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6903
 PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="C6903-user 5.1.1 14.6.A.1.236 2031203603 release-keys"
 
@@ -12,8 +13,11 @@ TARGET_GAPPS_ARCH := arm
 TARGET_DENSITY := xxhdpi
 TARGET_BOOT_ANIMATION_RES := 1080
 
+PRODUCT_GMS_CLIENTID_BASE := android-sony
+
 PRODUCT_NAME := aosp_honami
 PRODUCT_DEVICE := honami
+
 
 # AICP Device Maintainer
 # PRODUCT_BUILD_PROP_OVERRIDES += \
