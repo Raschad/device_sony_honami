d0cfce26d23b53ff182beab16aa25d0f2f729a86
Raschad <Raschad@yandex.ru>
2018-12-19 07:57:56 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,7 +19,7 @@ include device/sony/rhine-common/BoardConfigCommon.mk
 #-include vendor/sony/honami/BoardConfigVendor.mk
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := C6903,honami,homani
+TARGET_OTA_ASSERT_DEVICE := C6903,honami
 
 TARGET_SPECIFIC_HEADER_PATH += device/sony/honami/include
 
