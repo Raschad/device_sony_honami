8d9acc0b141fb0dca91215c3bdbe5936534ce2f9
Raschad <Raschad@yandex.ru>
2018-12-31 21:06:12 +0300

    Update bdroid_buildcfg.h

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -18,7 +18,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Xperia Z1"
+#define BTM_DEF_LOCAL_NAME   "Xperia Z Ultra"
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 
 // Enables Interleave scan
