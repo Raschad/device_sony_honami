aa91e798c834bf2187daf7be147da15c440a0ed8
Raschad <Raschad@yandex.ru>
2018-12-31 20:58:46 +0300

    Update aosp_honami.mk

--- aosp_honami.mk
+++ aosp_honami.mk
@@ -1,13 +1,13 @@
 # Inherit AICP common Phone stuff.
 $(call inherit-product, vendor/aosp/common.mk)
 
-$(call inherit-product, device/sony/honami/full_honami.mk)
+$(call inherit-product, device/sony/honami/full_togari.mk)
 
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6903
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="C6903-user 5.1.1 14.6.A.1.236 2031203603 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6833
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="C6833-user 5.1.1 14.6.A.1.236 2031203603 release-keys"
 
-BUILD_FINGERPRINT := Sony/C6903/C6903:5.1.1/14.6.A.1.236/2031203603:user/release-keys
+BUILD_FINGERPRINT := Sony/C6833/C6833:5.1.1/14.6.A.1.236/2031203603:user/release-keys
 
 # TARGET_GAPPS_ARCH := arm
 # TARGET_DENSITY := xxhdpi
@@ -15,8 +15,8 @@ BUILD_FINGERPRINT := Sony/C6903/C6903:5.1.1/14.6.A.1.236/2031203603:user/release
 
 # PRODUCT_GMS_CLIENTID_BASE := android-sony
 
-PRODUCT_NAME := aosp_honami
-PRODUCT_DEVICE := honami
+PRODUCT_NAME := aosp_togari
+PRODUCT_DEVICE := togari
 
 
 # AICP Device Maintainer
