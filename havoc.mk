7167b909e159745937ecd607c6a2159cad46ed96
Raschad <Raschad@yandex.ru>
2019-01-02 21:24:15 +0300

    Update and rename havoc_honami.mk to havoc.mk

--- havoc.mk
+++ havoc.mk
@@ -15,7 +15,7 @@ BUILD_FINGERPRINT := Sony/C6903/C6903:5.1.1/14.6.A.1.236/2031203603:user/release
 
 # PRODUCT_GMS_CLIENTID_BASE := android-sony
 
-PRODUCT_NAME := aosp_honami
+PRODUCT_NAME := havoc_honami
 PRODUCT_DEVICE := honami
 
 
