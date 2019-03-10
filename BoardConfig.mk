7bd9afe261a8f83a409779ca5394cf27952cc6f4
Raschad <Raschad@yandex.ru>
2019-03-10 20:03:03 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# SELinux (must be specified before including common mk files)
+BOARD_USE_ENFORCING_SELINUX := true
+
 # inherit from the common rhine definitions
 include device/sony/rhine-common/BoardConfigCommon.mk
 
