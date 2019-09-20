9e29489bfa82f1079989cf267420a859a42861dd
Raschad <Raschad@yandex.ru>
2019-09-20 22:15:49 +0300

    fix call *.mk

--- honami.mk
+++ honami.mk
@@ -40,7 +40,7 @@ PRODUCT_COPY_FILES += \
    $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
 # Properties
-$(call inherit-product, device/sony/honami/system_prop.mk
+$(call inherit-product, device/sony/honami/system_prop.mk)
 
 # Sensors
 PRODUCT_COPY_FILES += \
