fa64b45884b3528d5ba8bb2ed4b3863da73c98d6
Raschad <Raschad@yandex.ru>
2019-09-21 21:16:07 +0300

    add arrow config

---
+++ arrow_honami.mk
@@ -0,0 +1,24 @@
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/arrow/config/common.mk)
+
+$(call inherit-product, device/sony/honami/full_honami.mk)
+
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6903
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="C6903-user 5.1.1 14.6.A.1.236 2031203603 release-keys"
+
+BUILD_FINGERPRINT := Sony/C6903/C6903:5.1.1/14.6.A.1.236/2031203603:user/release-keys
+
+TARGET_DENSITY := xxhdpi
+TARGET_BOOT_ANIMATION_RES := 1080
+
+
+PRODUCT_GMS_CLIENTID_BASE := android-sony
+
+PRODUCT_NAME := arrow_honami
+PRODUCT_DEVICE := honami
+
+
+# Boot animation
+# TARGET_SCREEN_HEIGHT := 1920
+# TARGET_SCREEN_WIDTH := 1080
